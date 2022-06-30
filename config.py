a1ef9fc9ba07798ba26ae758a2a9bf01b5a07f87
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2022-06-30 19:39:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ from os import environ
 
 load_dotenv("config.env")
 
-BOT_TOKEN = environ.get("BOT_TOKEN")
-API_ID = int(environ.get("API_ID"))
-API_HASH = environ.get("API_HASH")
+BOT_TOKEN = "5439555827:AAElCvsZouahPVKrMuEU1_qNox587CY-HNQ"
+API_ID = "3188376"
+API_HASH = "4c187a62bc008363b48f7aa3ed38e56b"
 API_ID1 = int(environ.get("API_ID1"))
 API_HASH1 = environ.get("API_HASH1")
 SUDO_USERS_ID = environ.get("SUDO_USERS_ID")
-LOG_GROUP_ID = environ.get("LOG_GROUP_ID")
+LOG_GROUP_ID = "-1001174927319"
 BASE_DB = environ.get("BASE_DB")
-MONGO_URL = environ.get("MONGO_URL")
+MONGO_URL = "mongodb+srv://Athenabetta:Athenabetta@cluster0.gsgt15j.mongodb.net/?retryWrites=true&w=majority"
 ARQ_API_URL = environ.get("ARQ_API_URL")
-ARQ_API_KEY = environ.get("ARQ_API_KEY")
+ARQ_API_KEY = "VIXRLF-UTDWJU-LMTAIW-FMHCLU-ARQ"
 COMMAND_PREFIXES = environ.get("COMMAND_PREFIXES")
-F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL")
+F_SUB_CHANNEL = "Tamilcoders"
